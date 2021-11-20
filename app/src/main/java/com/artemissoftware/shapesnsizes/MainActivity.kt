f702a4d77e05df5c28895514754949d86739e01d
gustavo <gustavo.isaac@gmail.com>
2021-11-20 16:36:54 +0000

    Greeting text composable

--- app/src/main/java/com/artemissoftware/shapesnsizes/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/shapesnsizes/MainActivity.kt
@@ -3,11 +3,22 @@ package com.artemissoftware.shapesnsizes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.artemissoftware.shapesnsizes.ui.theme.ShapesNSizesTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +28,7 @@ class MainActivity : ComponentActivity() {
             ShapesNSizesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -25,14 +36,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Text(
+        text = "Lets test shapes and sizes",
+        style = typography.h3.copy(color = Color.Blue),
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(32.dp)
+            .border(width = 4.dp, color = Color.Blue, CutCornerShape(32.dp))
+            .graphicsLayer {
+                shadowElevation = 8.dp.toPx()
+                shape = CutCornerShape(32.dp)
+                clip = true
+            }
+            .background(color = Color.Green)
+            .padding(32.dp)
+
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ShapesNSizesTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
